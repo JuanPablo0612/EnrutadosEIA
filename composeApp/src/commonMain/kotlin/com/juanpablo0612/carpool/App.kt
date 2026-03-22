5bc14fd39a32a6c46e8ad194410df8df9b9b668b
JuanPablo0612 <gallegojuanpablo39@gmail.com>
2026-03-22 15:08:45 -0500

    refactor navigation structure; extract AppNavigation composable and streamline routing for login, registration, and home screens

--- composeApp/src/commonMain/kotlin/com/juanpablo0612/carpool/App.kt
+++ composeApp/src/commonMain/kotlin/com/juanpablo0612/carpool/App.kt
@@ -1,71 +1,26 @@
 package com.juanpablo0612.carpool
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.juanpablo0612.carpool.presentation.auth.login.LoginScreen
-import com.juanpablo0612.carpool.presentation.auth.login.LoginViewModel
-import com.juanpablo0612.carpool.presentation.auth.register.RegisterScreen
-import com.juanpablo0612.carpool.presentation.auth.register.RegisterViewModel
+import com.juanpablo0612.carpool.presentation.navigation.AppNavigation
 import com.juanpablo0612.carpool.presentation.ui.theme.CarpoolTheme
-import enrutadoseia.composeapp.generated.resources.Res
-import enrutadoseia.composeapp.generated.resources.home_welcome
-import org.jetbrains.compose.resources.stringResource
-import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
 fun App() {
     CarpoolTheme {
         val navController = rememberNavController()
-        val snackbarHostState = remember { SnackbarHostState() }
 
         Scaffold(
-            snackbarHost = { SnackbarHost(snackbarHostState) },
             contentWindowInsets = WindowInsets(0, 0, 0, 0)
         ) { padding ->
-            NavHost(
+            AppNavigation(
                 navController = navController,
-                startDestination = "login",
                 modifier = Modifier.padding(padding)
-            ) {
-                composable("login") {
-                    val viewModel: LoginViewModel = koinViewModel()
-                    LoginScreen(
-                        viewModel = viewModel,
-                        onLoginSuccess = {
-                            navController.navigate("home") {
-                                popUpTo("login") { inclusive = true }
-                            }
-                        },
-                        onNavigateToRegister = { navController.navigate("register") }
-                    )
-                }
-                composable("register") {
-                    val viewModel: RegisterViewModel = koinViewModel()
-                    RegisterScreen(
-                        viewModel = viewModel,
-                        onRegisterSuccess = {
-                            navController.navigate("home") {
-                                popUpTo("login") { inclusive = true }
-                            }
-                        },
-                        onNavigateToLogin = { navController.popBackStack() }
-                    )
-                }
-                composable("home") {
-                    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-                        Text(text = stringResource(Res.string.home_welcome))
-                    }
-                }
-            }
+            )
         }
     }
 }
---
+++ composeApp/src/commonMain/kotlin/com/juanpablo0612/carpool/presentation/navigation/Navigation.kt
@@ -0,0 +1,83 @@
+package com.juanpablo0612.carpool.presentation.navigation
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.juanpablo0612.carpool.presentation.auth.login.LoginScreen
+import com.juanpablo0612.carpool.presentation.auth.login.LoginViewModel
+import com.juanpablo0612.carpool.presentation.auth.register.RegisterScreen
+import com.juanpablo0612.carpool.presentation.auth.register.RegisterViewModel
+import enrutadoseia.composeapp.generated.resources.Res
+import enrutadoseia.composeapp.generated.resources.home_welcome
+import kotlinx.serialization.Serializable
+import org.jetbrains.compose.resources.stringResource
+import org.koin.compose.viewmodel.koinViewModel
+
+@Serializable
+sealed interface Route {
+    @Serializable
+    data object Login : Route
+
+    @Serializable
+    data object Register : Route
+
+    @Serializable
+    data object Home : Route
+}
+
+@Composable
+fun AppNavigation(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    NavHost(
+        navController = navController,
+        startDestination = Route.Login,
+        modifier = modifier
+    ) {
+        composable<Route.Login> {
+            val viewModel: LoginViewModel = koinViewModel()
+            LoginScreen(
+                viewModel = viewModel,
+                onLoginSuccess = {
+                    navController.navigate(Route.Home) {
+                        popUpTo(Route.Login) { inclusive = true }
+                    }
+                },
+                onNavigateToRegister = {
+                    navController.navigate(Route.Register)
+                }
+            )
+        }
+
+        composable<Route.Register> {
+            val viewModel: RegisterViewModel = koinViewModel()
+            RegisterScreen(
+                viewModel = viewModel,
+                onRegisterSuccess = {
+                    navController.navigate(Route.Home) {
+                        popUpTo(Route.Login) { inclusive = true }
+                    }
+                },
+                onNavigateToLogin = {
+                    navController.popBackStack()
+                }
+            )
+        }
+
+        composable<Route.Home> {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(text = stringResource(Res.string.home_welcome))
+            }
+        }
+    }
+}
