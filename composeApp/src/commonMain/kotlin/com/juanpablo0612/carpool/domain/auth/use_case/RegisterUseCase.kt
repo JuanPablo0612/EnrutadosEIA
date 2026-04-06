69fa5a807d99adbe262a98ccc7d0b64f72c2a6a2
JuanPablo0612 <gallegojuanpablo39@gmail.com>
2026-04-06 14:53:01 -0500

    refactor: remove unused imports from authentication use cases

--- composeApp/src/commonMain/kotlin/com/juanpablo0612/carpool/domain/auth/use_case/RegisterUseCase.kt
+++ composeApp/src/commonMain/kotlin/com/juanpablo0612/carpool/domain/auth/use_case/RegisterUseCase.kt
@@ -1,8 +1,5 @@
 package com.juanpablo0612.carpool.domain.auth.use_case
 
-import com.juanpablo0612.carpool.core.result.AppResult
-import com.juanpablo0612.carpool.domain.auth.model.AuthError
-import com.juanpablo0612.carpool.domain.auth.model.User
 import com.juanpablo0612.carpool.domain.auth.repository.AuthRepository
 
 class RegisterUseCase(private val repository: AuthRepository) {
