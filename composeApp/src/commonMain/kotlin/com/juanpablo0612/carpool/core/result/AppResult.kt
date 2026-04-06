f4ae0ffd03c8fcd063bffcf139bdb5682ea6a068
JuanPablo0612 <gallegojuanpablo39@gmail.com>
2026-04-06 12:47:02 -0500

    remove core utility classes; delete redundant `AppResult`, `Resource`, and `DispatcherProvider` implementations from the core package

--- composeApp/src/commonMain/kotlin/com/juanpablo0612/carpool/core/result/AppResult.kt
+++
@@ -1,6 +0,0 @@
-package com.juanpablo0612.carpool.core.result
-
-sealed class AppResult<out T, out E> {
-    data class Success<out T>(val data: T) : AppResult<T, Nothing>()
-    data class Error<out E>(val error: E) : AppResult<Nothing, E>()
-}
