cd41884422e17d7ac2e5c9e1cb50e59e4ed93c0f
JuanPablo0612 <gallegojuanpablo39@gmail.com>
2026-04-23 22:22:11 -0500

    feat: update PlaceSelectorViewModel; add flow collection for improved state management

--- composeApp/src/commonMain/kotlin/com/juanpablo0612/carpool/presentation/places/selector/PlaceSelectorViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/juanpablo0612/carpool/presentation/places/selector/PlaceSelectorViewModel.kt
@@ -8,6 +8,7 @@ import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
